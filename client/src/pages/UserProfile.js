8b1042542b89a45303c867608e36c8d7f009aaa8
Mona Lisa <cjensen@netechnical.com>
2023-04-10 10:43:46 -0500

    fixed syntax error in queries.js, updated UserProfile.js to add fetching code; created a UserDetails.js component to pull other user's data from; and added additional explaination text on the LandingPage.js

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect, useSt
+ } from "react";
 import { useQuery, useMutation } from "@apollo/client";
 import { ALL_DOGS } from "../utils/queries";
+import UserDetails from '../components/UserDetails';
 
-export default function UserProfile() {
-  const { loading, data } = useQuery(ALL_DOGS);
-  const otherDogs = data?.profiles || [];
+const userProfile = () => {
+  const [profiles, setProfiles] = useState(null)
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  useEffect(() => {
+    const fetchProfiles = async () => {
+      const response = await fetch('/api/profiles')
+      const json = await response.json()
+      
+      if (response.ok) {
+        setProfiles(json)
+      }
+    }
+
+      fetchProfiles()
+  }, [])
+
+
+
+// export default function UserProfile() {
+//   const { loading, data } = useQuery(ALL_DOGS);
+//   const otherDogs = data?.profiles || [];
+
+//   if (loading) {
+//     return <div>Loading...</div>;
+//   }
   return (
     <div className="flex-row justify-center">
       <div className="col-12 col-md-10 my-3">
-        <h1>Who do you want to go on a doggy date with?</h1>
+        {/* <h1>Who do you want to go on a doggy date with?</h1>
       </div>
       <div>
         <p>Meet {otherDogs.name}! </p>
@@ -21,8 +42,13 @@ export default function UserProfile() {
           {otherDogs.name} is a {otherDogs.breed}. {otherDogs.description}
         </p>
         //need to add query for location
-        <button>Click here to setup a time to play!</button>
+        <button>Click here to setup a time to play!</button> */}
+        {profiles && profiles.map((profile) => (
+          <UserDetails key={profile._id} profile={profile} />
+        ))}
       </div>
     </div>
   );
 }
+
+export default userProfile;
