e4b439bcdd5b510a7c173daadd3b279045f8a425
Benmarz10 <benjaminmarz10@gmail.com>
2023-04-13 19:39:50 -0500

    styled removed btn

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,35 @@
-import React from 'react';
-import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import SignupForm from './components/SignupForm'
-import LandingPage from './pages/LandingPage';
-import Dashboard from './pages/Dashboard';
-import UserProfile from './pages/UserProfile';
-import ViewMyProfile from './pages/MyProfile';
-import Navbar from './components/Navbar';
-import QueryTest from './components/Test'
+import SignupForm from "./components/SignupForm";
+import LandingPage from "./pages/LandingPage";
+import Dashboard from "./pages/Dashboard";
+import UserProfile from "./pages/UserProfile";
+import ViewMyProfile from "./pages/MyProfile";
+import Navbar from "./components/Navbar";
+import QueryTest from "./components/Test";
 
 //main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 //middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -35,39 +40,23 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-
 function App() {
   return (
     <ApolloProvider client={client}>
-    <Navbar></Navbar>
-    <Router>
-       
-
+      <Navbar></Navbar>
+      <Router>
         <Routes>
-              <Route 
-                path="/" 
-                element={<LandingPage/>} 
-              />
-              <Route 
-                path="/signup" 
-                element={<SignupForm/>} 
-              />
-
-       
-          <Route  path='/myprofile' element={<ViewMyProfile/>} />
-          <Route  path='/userprofile' element={<UserProfile/>} />
-          <Route  path='/dashboard' element={<Dashboard/>} />
-          <Route path='/test' element={<QueryTest/>} /> 
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signup" element={<SignupForm />} />
+          <Route path="/myprofile" element={<ViewMyProfile />} />
+          <Route path="/userprofile" element={<UserProfile />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/test" element={<QueryTest />} />
           <Route render={() => <h1>Wrong page!</h1>} />
-    
-      
-      
-      </Routes>
-    </Router>
-    
-     </ApolloProvider>
+        </Routes>
+      </Router>
+    </ApolloProvider>
   );
 }
 
 export default App;
-
